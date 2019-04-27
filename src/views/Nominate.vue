df2d23a6bbf51a325e7813d4f397461b6261f79c
Jen Looper <jen.looper@gmail.com>
2019-04-26 20:24:06 -0400

    note, validation isn't quite there

--- src/views/Nominate.vue
+++ src/views/Nominate.vue
@@ -19,11 +19,50 @@
         <v-container>
           <v-layout row wrap>
             <v-flex lg6 offset-lg2>
-              <v-text-field v-model="name" name="name" label="Name"></v-text-field>
+              <v-text-field required :rules="rules" v-model="name" name="name" label="Name"></v-text-field>
             </v-flex>
-
-            <v-btn type="submit">Submit</v-btn>
           </v-layout>
+          <v-layout row wrap>
+            <v-flex lg6 offset-lg2>
+              <v-text-field
+                required
+                :rules="rules"
+                v-model="momName"
+                name="momName"
+                label="Mom's Name (if you're nominating yourself, leave this blank)"
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-container fluid>
+            <v-radio-group v-model="momType" :mandatory="true" :rules="rules">
+              <v-radio label="I'm a Mom who's a Developer" value="devMom"></v-radio>
+              <v-radio label="I'm nominating a Mom who's a Developer" value="myMom"></v-radio>
+              <v-radio label="I'm a Fur Mom" value="furMom"></v-radio>
+            </v-radio-group>
+          </v-container>
+          <v-layout row wrap>
+            <v-flex lg6 offset-lg2>
+              <v-text-field
+                required
+                :rules="rules"
+                v-model="picUrl"
+                name="picUrl"
+                label="URL of a photo you'd like to show"
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row wrap>
+            <v-flex lg6 offset-lg2>
+              <v-textarea
+                required
+                :rules="rules"
+                v-model="momBio"
+                name="momBio"
+                label="Mom's bio. What would you like to celebrate?"
+              ></v-textarea>
+            </v-flex>
+          </v-layout>
+          <v-btn type="submit">Submit</v-btn>
         </v-container>
       </v-form>
     </v-card>
@@ -35,9 +74,11 @@ export default {
   data() {
     return {
       name: "",
-      form: {
-        name: ""
-      }
+      momName: "",
+      momBio: "",
+      picUrl: "",
+      momType: "",
+      rules: [v => !!v || "This field is required"]
     };
   },
   methods: {
@@ -49,14 +90,15 @@ export default {
         .join("&");
     },
     handleSubmit() {
-      //console.log(this.encode(this.name));
       fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: this.encode({
           "form-name": "mominate",
-          //...this.form
-          name: this.name
+          name: this.name,
+          momName: this.momName,
+          momBio: this.momBio,
+          picUrl: this.picUrl
         })
       })
         .then(() => {
